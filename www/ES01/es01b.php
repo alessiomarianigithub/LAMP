9e05064cda1a1bd8961801791d6b8d79b1b31592
alessiomarianigithub alessiomarianigitpod <alessiogithub@gmail.com>
2024-11-07 08:12:19 +0000

    a

---
+++ www/ES01/es01b.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang >
+<head>
+<title>Esercizio B</title>
+</head>
+<body>
+    <?php
+        $name = "Paolo";
+
+       
+        $today = new DateTime("now", new DateTimeZone('Europe/Rome'));
+        $hour = $today->format('H');
+
+       
+        if ($hour >= 8 && $hour < 12) 
+        {
+            $greeting = "Buongiorno";
+        } elseif ($hour >= 12 && $hour < 20)
+        {
+            $greeting = "Buonasera";
+        } else {
+            $greeting = "Buonanotte";
+        }
+
+        $browser = $_SERVER['HTTP_USER_AGENT'];
+
+        
+        echo "$greeting $name, benvenuta nella mia prima pagina PHP!<br>";
+        echo "Stai usando il browser: $browser";
+    ?>
